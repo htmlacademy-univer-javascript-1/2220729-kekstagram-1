69083debde22b31f3b7a3f1233f38429d633ce00
Denis <gaz91.91@mail.ru>
2022-12-12 10:19:21 +0500

    Сделал задание 21

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
-import './util.js';
-// import {createImages} from './data.js';
+import {debounce} from './util.js';
 import {createThumbnails} from './thumbnails.js';
 import {setUserFormSubmit, closeUploadFileForm} from './form.js';
 import {getData} from './api.js';
 import './big-pictures.js';
 import {showError, showSuccess} from './alerts.js';
-// import {showAlert} from './util';
+import {setFilter, showFilters, TIMEOUT_DELAY} from './filters.js';
 
-// createThumbnails(createImages());
-getData(createThumbnails);
+getData((data) => {
+  createThumbnails(data);
+  showFilters();
+  setFilter(debounce((filterData) => createThumbnails(filterData(data)), TIMEOUT_DELAY));
+});
 
 setUserFormSubmit(() => {
   closeUploadFileForm();
@@ -17,5 +19,3 @@ setUserFormSubmit(() => {
   closeUploadFileForm(null, false);
   showError();
 });
-// showSuccess();
-// createThumbnails();
